2f549e4311d448789d1e5d77e23035cc89556ab0
Bruno Verdan <bruno.verdan@al.infnet.edu.br>
2020-08-18 15:44:35 -0300

    Método para sessão.

--- Tp1_WebApi/Controllers/AmigosController.cs
+++ Tp1_WebApi/Controllers/AmigosController.cs
@@ -28,27 +28,22 @@ namespace Tp1_WebApi.Controllers
         public ActionResult Index(string ids)
         {
 
-            if (!String.IsNullOrWhiteSpace(ids))
-            {
-                foreach (var item in ids.Split(","))
-                {
-                    amigosSelecionados.Add(_amigoService.FindAllById(int.Parse(item)));
-                }
-            }
-
-            this.HttpContext.Session.SetString("AmigosSelecionados", JsonConvert.SerializeObject(amigosSelecionados));
-
-            if (this.HttpContext.Session.GetString("AmigosSelecionados") != null)
-            {
-                var idsSelecionados = JsonConvert.DeserializeObject<List<Amigos>>(this.HttpContext.Session.GetString("AmigosSelecionados"));
-                ViewBag.IdsSelecionados = idsSelecionados.Select(x => x.Id.ToString());
-            }
+            MantendoSessão(ids);
             var list = _amigoService.FindAll();
             return View(list);
 
         }
 
         public ActionResult AmigosEmail(string ids)
+        {
+            MantendoSessão(ids);
+
+            var list = _amigoService.FindAll();
+            return View(list);
+        }
+
+
+        private void MantendoSessão(string ids)
         {
             if (!String.IsNullOrWhiteSpace(ids))
             {
@@ -65,9 +60,6 @@ namespace Tp1_WebApi.Controllers
                 var idsSelecionados = JsonConvert.DeserializeObject<List<Amigos>>(this.HttpContext.Session.GetString("AmigosSelecionados"));
                 ViewBag.IdsSelecionados = idsSelecionados.Select(x => x.Id.ToString());
             }
-
-            var list = _amigoService.FindAll();
-            return View(list);
         }
     }
 }
